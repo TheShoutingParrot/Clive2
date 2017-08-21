6556d6f464b77750ff4ed278259213d53a172ba9
TheShoutingParrot <vxoca2005@gmail.com>
2017-08-21 20:57:38 +0300

    Add files via upload

---
+++ Clive2/clivebrain.py
@@ -0,0 +1,55 @@
+from cliveheart import *
+
+say("Welcome human, my name is Clive")
+
+
+def Main():
+  input = raw_input("Q:  ")
+
+  if input == '':
+    say0('You didint enter a question')
+    print('\n\n')
+
+  else:
+    print('\n\n')
+    
+    input = input.lower()
+
+    originalinput = input #Trust me this is important
+
+    config.x = input  #This creates a module wide variable variable
+
+    try:
+      personalquestions()
+    
+    except:
+      try:
+            #wordnick
+            input = input.split(' ')
+            input = " ".join(input[2:])
+
+            answer = wordApi.getDefinitions(input, limit=1)
+
+            say('Definition: ' + answer[0].text)
+            print('\n(According to wordnick https://www.wordnik.com/)\n')
+      
+      except:
+          input = originalinput
+          try:
+              #wolframalpha
+              res = wolfram_client.query(input)   
+              answer = next(res.results).text
+          
+              say(answer)
+
+          except:
+                  #wikipedia
+                  try:
+                      input = input.split(' ')
+                      input = " ".join(input[2:])
+                      say(wikipedia.summary(input, sentences=2))
+
+                      print('\n(According to wikipedia, link: '+wikipedia.page(input).url+')')
+                      
+                  except:
+                      say('Sorry I did not understand!')
---
+++ Clive2/clivemouth.py
@@ -0,0 +1,9 @@
+from cliveheart import *
+from espeak import espeak
+
+def say(text):
+    print(text)
+    espeak.synth(text)
+
+def say0(text):
+    espeak.synth(text)
