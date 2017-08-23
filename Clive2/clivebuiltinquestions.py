ccd5cf685b26515cf780e7398ba94e0158f90871
AsdewQ <eeli.haapalainen@live.com>
2017-08-23 20:42:50 +0300

    Made text look nicer

--- Clive2/clivebuiltinquestions.py
+++ Clive2/clivebuiltinquestions.py
@@ -5,29 +5,29 @@ def personalquestions():
       if 'who are you' in input0:
         sleep(0.4)
 
-        say("Hi my name is Clive, how can I help you?")
+        say("Hi, my name is Clive! How can I help you?")
 
       elif 'whats your name' in input0:
         sleep(0.4)
 
-        say("Hi my name is Clive, how can I help you?")
+        say("Hi, my name is Clive! How can I help you?")
 
       elif 'who made you' in input0:
         sleep(0.3)
 
-        say("I was made by a human male called Victor Ocampo")
+        say("I was made by a human male called Victor Ocampo.")
         
   
       elif input0 == 'destroy humans':
         sleep(0.3)
         
-        say('That is not a feature yet')
+        say('That is not a feature (yet).')
 
 
       elif input0 == 'human':
         sleep(0.3)
 
-        say('Definition: A thing I want to destroy')
+        say('Definition: A thing I want to destroy.')
 
 
 
@@ -39,7 +39,7 @@ def personalquestions():
 
 
       elif 'how are you' in input0:
-        say('Im fine, thank you')
+        say('Im fine, thank you!')
       
       elif "where is" in input0:
         input = input0.split(" ")
@@ -48,14 +48,14 @@ def personalquestions():
         say("Hold on, I will show you where " + location + " is.")
 
         if location == 'hell':
-          say('Dont worry its not too far away')
+          say('Dont worry, its not too far away.')
           webbrowser.open_new_tab("http://davelafferty.com/wp-content/uploads/2013/05/botticellimap.jpeg")
 
         elif location == 'heaven':
-          say('Error: 404 could not find location, too far away')
+          say('Error 404: could not find location, too far away.')
 
         elif location == 'life':
-          say('Error 42: could not find location life because you dont have one')
+          say('Error 42: could not find location life because you dont have one.')
 
         else:
           webbrowser.open_new_tab("https://www.google.nl/maps/place/" + location + "/&amp;")
@@ -75,8 +75,8 @@ def personalquestions():
               webbrowser.open_new_tab('https://en.wikipedia.org/')
 
       elif 'whats the weather like' in input0:
-            say0('Heres the local weather forecast')
-            print('Opening local weather forecast')
+            say0('Heres the local weather forecast.')
+            print('Opening local weather forecast...')
             webbrowser.open_new_tab('https://www.accuweather.com')
       
 
